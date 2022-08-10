2ce1a74419a187fc530125ead363a45a4bba4ed4
RogerNoel <noel.roger.pro@gmail.com>
2022-08-10 22:06:51 +0200

    petit exercice + commentaires

--- converter.py
+++ converter.py
@@ -1,34 +1,44 @@
 from tkinter import *
+# -> submodule de Tkinter permettant l'usage des 'themed widgets' de TK V8.5
 from tkinter import ttk
 
+# fonction(s), à déclarer au début
 
-def calculate():
+
+def calculate(*args):
     try:
         value = float(valeur_entree.get())
-        resultat.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        affichage_resultat.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
 
 
+# Fenêtre principale de l'application
 root = Tk()
 root.title("Convertisseur pieds en mètres")
-# définition du cadre principal
+
+# définition du cadre principal qui contiendra les objets de l'interface
 cadre_principal = ttk.Frame(
     root, padding="3 3 140 10")  # nord, sud, est, ouest
 cadre_principal.grid(column=0, row=0, sticky=(N, W, E, S))
+#   les deux lignes suivantes permettent au cadre principal de s'adapter
+#   quand la fenêtre change de taille.
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
 valeur_entree = StringVar()
-# -> input box
-feet_entry = ttk.Entry(cadre_principal, width=10,
-                       textvariable=valeur_entree)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+# -> input box: 1) on le créé et 2) on le place sur l'interface
+txt_valeurAConvertir = ttk.Entry(
+    cadre_principal, width=10, textvariable=valeur_entree)
+txt_valeurAConvertir.grid(column=2, row=1, sticky=(W, E))
+
+# Les widgets suivants ne sont pas stockés dans des variables
+# Comme celui au-dessus, on les créé et on les place
 
 # -> label du résultat à afficher
-resultat = StringVar()
-ttk.Label(cadre_principal, textvariable=resultat, borderwidth=1, relief="solid").grid(
-    column=2, row=2, sticky=(W, E))
+affichage_resultat = StringVar()
+ttk.Label(cadre_principal, textvariable=affichage_resultat,
+          borderwidth=1, relief="solid").grid(column=2, row=2, sticky=(W, E))
 
 # -> bouton "calculer"
 ttk.Button(cadre_principal, text="Calculer", command=calculate).grid(
@@ -44,7 +54,7 @@ ttk.Label(cadre_principal, text="mètre(s)").grid(column=3, row=2, sticky=W)
 for child in cadre_principal.winfo_children():
     child.grid_configure(padx=10, pady=3)
 
-feet_entry.focus()
-root.bind("<Return>", calculate)
+txt_valeurAConvertir.focus()
+root.bind("<Return>", calculate)  # assigne la fonction à la touche "return"
 
 root.mainloop()
--- interfaces.py
+++ interfaces.py
@@ -1,6 +1,22 @@
 from tkinter import *
 from tkinter import ttk
+
+
+def afficher(*args):
+    try:
+        texte_a_afficher.set("Salut")
+    except ValueError:
+        pass
+
+
 root = Tk()
 ttk.Button(root, text="Hello World").grid()
 ttk.Checkbutton(root, text="cocher").grid()
+ttk.Button(root, text='Affichage', command=afficher).grid()
+texte_a_afficher = StringVar()
+ttk.Label(root, textvariable=texte_a_afficher, width=12,
+          borderwidth=2, relief='solid').grid()
+
+for child in root.winfo_children():
+    child.grid_configure(padx=10, pady=3)
 root.mainloop()
