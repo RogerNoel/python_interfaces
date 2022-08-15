29348d75058bfa3d20445078f9136fee5971a2f7
RogerNoel <noel.roger.pro@gmail.com>
2022-08-15 23:23:01 +0200

    mise côte à côte des frames 2 et 3

--- widgets.py
+++ widgets.py
@@ -12,21 +12,20 @@ cadrePrincipal = ttk.Frame(root, padding=10, width=400,
                            height=400, borderwidth=2, relief='sunken')
 cadrePrincipal.grid(column=0, row=0, sticky=(N, W, E, S))
 
-ttk.Label(cadrePrincipal, text="Revue des widgets",
-          width=20).grid()
+ttk.Label(cadrePrincipal, width=32,
+          text="Revue des widgets (cadre principal)").grid()
 
 # 2 frames à mettre côte à côte
 #   frame a
 cadre2 = ttk.Frame(cadrePrincipal, padding=5, width=100,
                    height=150, borderwidth=2, relief='sunken')
-cadre2.grid()
+cadre2.grid(column=0, row=1)
 img_fox = PhotoImage(file='fox.gif')
 lbl_cadre2 = ttk.Label(cadre2, image=img_fox).grid()
 #   frame b
 cadre3 = ttk.Frame(cadrePrincipal, padding=5, width=150,
                    height=100, borderwidth=2, relief='sunken')
-cadre3.grid()
+cadre3.grid(column=1, row=1)
 lbl_cadre3 = ttk.Label(cadre3, text='Cadre 3').grid()
-
 
 root.mainloop()
